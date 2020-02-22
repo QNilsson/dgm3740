6afd43dd27b54d013e1f387717f63fdcb8d7b216
Quinn <quinn.nilsson@gmail.com>
2020-02-21 19:48:54 -0700

    being annoying

--- js/activate.js
+++ js/activate.js
@@ -3,7 +3,7 @@
 "use strict";
 let found = false;
 (function () {
-let elements = document.querySelectorAll("#primaryNav li a");
+let elements = document.querySelectorAll("primaryNav li a");
 let getPageName = function (str) {
     let queryIgnore = /\/+|[?\#].*$/;
     let parts = str.split(queryIgnore);
